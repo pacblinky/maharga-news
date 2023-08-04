9e407aec59ba1d5f600d16a43f20926840b4e02a
pacblinky <40526207+pacblinky@users.noreply.github.com>
2023-08-04 23:39:49 +0300

    save for now before electircity outage

--- main.py
+++ main.py
@@ -4,36 +4,56 @@ import threading
 import time
 import random
 
+loop = False
+
+class DeviceThread(threading.Thread):
+    def __init__(self, device, master : ADBMaster):
+        threading.Thread.__init__(self)
+        self.device = device
+        self.master = master
+
+    def run(self):
+        global loop
+        self.master.toggleWifi(False, self.device)
+        self.master.toggleData(True, self.device)
+        while loop:
+            while(True and loop == True):
+                if self.master.isConnected(self.device):
+                    break
+            self.master.browse("https://3d-sof2.com", self.device)
+            time.sleep(5)
+
 class App(tk.Tk):
     def __init__(self):
-        self.loop = False
         super().__init__()
         self.title('Maharga Views Bot')
         self.start_btn = tk.Button(self,text="Start", command=lambda:threading.Thread(target=self.start).start())
         self.close_btn = tk.Button(self,text="Stop", command=lambda:threading.Thread(target=self.stop).start(), state="disabled")
         self.start_btn.grid(row=0, column=0)
         self.close_btn.grid(row=1, column=0)
+        self.threads = []
 
     def start(self):
+        global loop
+        loop = True
         master = ADBMaster()
         master.start()
         self.start_btn.configure(state="disabled")
         self.close_btn.configure(state="active")
-        self.loop = True
-        master.toggleWifi(False)
-        master.toggleData(True)
-        while(self.loop):
-            while(True and self.loop == True):
-                if master.isConnected():
-                    break
-            master.browse("https://maharga.online")
-            time.sleep(6)
-            master.toggleAirPlane()
-            master.toggleAirPlane()
-            
+        devices = master.client.devices()
+        if len(devices) <= 0:
+            print("No connected devices found")
+        else:
+            for device in devices:
+                thread =  DeviceThread(device, master)
+                self.threads.append(thread)
+            for thready in self.threads:
+                thready.start()
+            print(f"Started with {len(self.threads)} devices")
 
     def stop(self):
-        self.loop = False
+        global loop
+        loop = False
         self.start_btn.configure(state="active")
         self.close_btn.configure(state="disabled")
   
